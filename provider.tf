aec99ff05a2431b93d7ffe7ac9fc94597e98ed42
jshahid21 <jshahid21@github.com>
2019-04-18 16:27:31 -0400

    update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
 provider "oci" {
   region = "us-ashburn-1"
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaa5txfxrudc5ezitovlpjznw6b3l6sqvkzddepsfu4es2i4x2xyeia"
+  user_ocid = "ocid1.user.oc1..aaaaaaaagf2r3i3id6lasstuhcuaydr7zg6g2ww4swujpiqrinj4ukrywaoq"
+  fingerprint = "00:16:9a:4b:c6:99:bc:10:ca:25:cb:0e:2f:5b:bd:87"
 }
