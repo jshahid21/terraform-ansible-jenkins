f78e789f3a9cac4fda4a55d47df8f196bada848a
jshahid21 <jshahid21@github.com>
2019-04-18 12:57:43 -0400

    add

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-# variable "tenancy_ocid" {}
-# variable "user_ocid" {}
-# variable "fingerprint" {}
-# variable "private_key_path" {}
-# variable "region" {}
-#
-# variable "compartment_ocid" {}
-# variable "ssh_public_key" {}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+
+variable "compartment_ocid" {}
+variable "ssh_public_key" {}
 
 # Choose an Availability Domain
 variable "AD" {
