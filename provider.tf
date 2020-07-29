67891b76cc554ad1f480b3c4f0d88b97d8f0df7e
jshahid21 <jshahid21@hotmail.com>
2020-07-29 13:06:45 -0400

    no update

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "oci" {
   tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaa5txfxrudc5ezitovlpjznw6b3l6sqvkzddepsfu4es2i4x2xyeia"
   user_ocid = "ocid1.user.oc1..aaaaaaaagf2r3i3id6lasstuhcuaydr7zg6g2ww4swujpiqrinj4ukrywaoq"
-  fingerprint = "00:16:9a:4b:c6:99:bc:10:ca:25:cb:0e:2f:5b:bd:87"
-  private_key_path = "/var/lib/jenkins/.ocijs/oci_api_key_js.pem"
+  fingerprint = "5c:97:a9:3d:83:c1:38:a0:9e:21:aa:91:76:da:10:7f"
+  private_key_path = "/Users/jushahid/.oci/oci_api_key.pem"
   region = "us-ashburn-1"
 }
