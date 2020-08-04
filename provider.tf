c84f840bbe525eb72864647ca1b949d794bf478b
jshahid21 <jshahid21@hotmail.com>
2020-08-04 11:15:21 -0400

    update

--- provider.tf
+++ provider.tf
@@ -1,8 +1,9 @@
-provider "oci" {
-  version          = ">= 3.27.0"
-  #tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaa7l6wfqydzz6otbjnhrloxf4tezittufwjlwbaoxvajvkr6hyxmba"
-  #user_ocid        = "ocid1.user.oc1..aaaaaaaaw4qarxn6kse22u3vqxrz36h3c6cet337lspz4rjozuejunjpg3fq"
-  #fingerprint      = "5c:97:a9:3d:83:c1:38:a0:9e:21:aa:91:76:da:10:7f"
-  #private_key_path = "/Users/jushahid/.oci/oci_api_key.pem"
-  #region           = "uk-london-1"
-}
+# provider "oci" {
+#   version          = ">= 3.27.0"
+#   tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaa7l6wfqydzz6otbjnhrloxf4tezittufwjlwbaoxvajvkr6hyxmba"
+#   user_ocid        = "ocid1.user.oc1..aaaaaaaaw4qarxn6kse22u3vqxrz36h3c6cet337lspz4rjozuejunjpg3fq"
+#   fingerprint      = "5c:97:a9:3d:83:c1:38:a0:9e:21:aa:91:76:da:10:7f"
+#   private_key_path = "/Users/jushahid/.oci/oci_api_key.pem"
+#   region           = "uk-london-1"
+# }
+provider "oci" {}
